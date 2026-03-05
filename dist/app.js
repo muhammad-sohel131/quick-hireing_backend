ec72ac7375c0788f2ae4e8bc0eaaba7248be2d0c
= <sohelf131@gmail.com>
2026-03-06 00:25:42 +0600

    added notFound Route middleware

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,7 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./app/routes"));
 const env_1 = require("./app/config/env");
 const globalErrorHandler_1 = require("./app/middlewares/globalErrorHandler");
+const notFound_1 = require("./app/middlewares/notFound");
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 // middlewares
@@ -18,7 +19,14 @@ app.use((0, cors_1.default)({
 }));
 app.use(express_1.default.json());
 // routes
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Quick Hiring API",
+    });
+});
 app.use("/api", routes_1.default);
 app.use(globalErrorHandler_1.globalErrorHandle);
+app.use(notFound_1.notFound);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import routes from "./app/routes";
 import { envVars } from "./app/config/env";
 import { globalErrorHandle } from "./app/middlewares/globalErrorHandler";
+import { notFound } from "./app/middlewares/notFound";
 
 const app: Application = express();
 app.use(cookieParser());
@@ -18,7 +19,14 @@ app.use(
 app.use(express.json());
 
 // routes
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Quick Hiring API",
+  });
+})
 app.use("/api", routes);
 
 app.use(globalErrorHandle);
+app.use(notFound)
 export default app;
